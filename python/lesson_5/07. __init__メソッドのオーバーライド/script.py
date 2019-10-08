19c5c1a7ca7a7ea16585a75ced4d155f41a4b1ab
knwldg <knwldg@gmail.com>
2019-10-08 10:30:15 +0900

    python lesson_5

---
+++ python/lesson_5/07. __init__メソッドのオーバーライド/script.py
@@ -0,0 +1,14 @@
+from food import Food
+from drink import Drink
+
+# Food()に引数を追加してください
+food1 = Food('サンドイッチ', 500, 330)
+
+# 以下の1行は削除してください
+# food1.calorie = 330
+
+print(food1.info())
+
+drink1 = Drink('コーヒー', 300)
+drink1.amount = 180
+print(drink1.info())
